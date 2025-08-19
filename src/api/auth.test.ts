f0e57d4ee62901b09fd72ddc74ee29911f360dce
Erbatax <samlarose1997@hotmail.ca>
2025-08-19 09:11:36 -0400

    Added Prettier

--- src/api/auth.test.ts
+++ src/api/auth.test.ts
@@ -1,36 +1,35 @@
-import { describe, expect, test } from "vitest"
-import { getAPIKey } from "./auth"
+import { describe, expect, test } from "vitest";
+import { getAPIKey } from "./auth";
 
 const person = {
-    isActive: true,
-    age: 32,
-}
+  isActive: true,
+  age: 32,
+};
 
 describe("person", () => {
-    test("person is defined", () => {
-        expect(person).toBeDefined()
-    })
+  test("person is defined", () => {
+    expect(person).toBeDefined();
+  });
 
-    test("is active", () => {
-        expect(person.isActive).toBeTruthy()
-    })
-})
-
+  test("is active", () => {
+    expect(person.isActive).toBeTruthy();
+  });
+});
 
 describe("getAPIKey", () => {
-    const headers = {
-        authorization: "ApiKey 1234567890",
-    }
+  const headers = {
+    authorization: "ApiKey 1234567890",
+  };
 
-    test("returns api key", () => {
-        expect(getAPIKey(headers)).toBe("1234567890")
-    })
+  test("returns api key", () => {
+    expect(getAPIKey(headers)).toBe("1234567890");
+  });
 
-    test("returns null if authorization header is missing", () => {
-        expect(getAPIKey({})).toBeNull()
-    })
+  test("returns null if authorization header is missing", () => {
+    expect(getAPIKey({})).toBeNull();
+  });
 
-    test("returns null if authorization header is invalid", () => {
-        expect(getAPIKey({ authorization: "Bearer 1234567890" })).toBeNull()
-    })
-})
+  test("returns null if authorization header is invalid", () => {
+    expect(getAPIKey({ authorization: "Bearer 1234567890" })).toBeNull();
+  });
+});
